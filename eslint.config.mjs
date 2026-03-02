13d2a903dc6e715f9fd3415f93d7602f4523fb14
DmytroRiabinin <dmitriy.riabinin@tech-stack.io>
2026-03-02 13:06:41 +0200

    fix for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import apiConfig from './apps/api/eslint.config.mjs';
 import webConfig from './apps/web/eslint.config.mjs';
 
-export default [
+const config = [
   ...apiConfig.map((c) => ({ ...c, files: c.files ?? ['apps/api/**/*.ts'] })),
   ...webConfig.map((c) => ({ ...c, files: c.files ?? ['apps/web/**/*.{ts,tsx}'] })),
 ];
+
+export default config;
