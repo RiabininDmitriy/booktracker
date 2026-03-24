b956e99437a8b60cd72f5a756e5633b0b8d14974
DmytroRiabinin <dmitriy.riabinin@tech-stack.io>
2026-03-24 12:29:32 +0200

    add server side pagination

--- apps/api/src/books/books.controller.ts
+++ apps/api/src/books/books.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BookSearchResultDto } from './dto/book-search-result.dto';
+import { BooksCatalogResponseDto } from './dto/books-catalog-item.dto';
+import { ListBooksDto } from './dto/list-books.dto';
 import { SearchBooksDto } from './dto/search-books.dto';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  list(@Query() query: ListBooksDto): Promise<BooksCatalogResponseDto> {
+    return this.booksService.list(query);
+  }
 
   @Get('search')
   search(@Query() query: SearchBooksDto): Promise<BookSearchResultDto[]> {
