768e6a6b50322c66519034521bd489f85fb4da0e
DmytroRiabinin <dmitriy.riabinin@tech-stack.io>
2026-03-27 12:55:10 +0200

    add redux

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+
+import { StoreProvider } from '@/lib/store/provider';
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
     </html>
   );
 }
