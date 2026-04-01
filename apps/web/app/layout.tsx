a26331d8b14879f28953f18a7f1880e5cbb4a27a
DmytroRiabinin <dmitriy.riabinin@tech-stack.io>
2026-04-01 16:37:45 +0300

    package update

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
