7e31740d856ec029252f12c1a27e208b4be709d9
DmytroRiabinin <dmitriy.riabinin@tech-stack.io>
2026-04-02 13:21:43 +0300

    add sst and deploy script

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,17 @@
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { configure as serverlessExpress } from '@vendia/serverless-express';
+import type { Callback, Context, Handler } from 'aws-lambda';
 import { ValidationError } from 'class-validator';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let server: Handler;
+
+async function setupApp(app: INestApplication) {
   const frontendOrigin = process.env.FRONTEND_ORIGIN ?? 'http://localhost:3000';
 
   app.enableCors({
@@ -45,8 +48,27 @@ async function bootstrap() {
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, swaggerDocument);
 
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 3001);
-  await app.listen(port);
+  return app;
 }
-void bootstrap();
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  if (!server) {
+    const app = await NestFactory.create(AppModule);
+    await setupApp(app);
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    server = serverlessExpress({ app: expressApp });
+  }
+  return server(event, context, callback);
+};
+
+if (!process.env.AWS_LAMBDA_FUNCTION_NAME) {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await setupApp(app);
+    const configService = app.get(ConfigService);
+    const port = configService.get<number>('PORT', 3001);
+    await app.listen(port);
+  }
+  void bootstrap();
+}
